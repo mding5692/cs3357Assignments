27a10975b513d4ec071702530a104629e262c4a7
Michael <m.ding5692@gmail.com>
2016-10-13 22:27:44 -0400

    Client.py update: Added instructions and cleaned up code

--- Asst2/client.py
+++ Asst2/client.py
@@ -7,11 +7,18 @@ print ("Attempting to contact server at ",TCP_IP,":",TCP_PORT)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 print ("Connection to Server Established")
-
-message = "What is the current date and time?"
-s.send(message)
+breakStr = "---------------------------------"
+print(breakStr)
+print("Instructions: Type 'What is the current date and time?' to get current time and date.")
+print("Else type 'exit' to stop server!")
+print(breakStr)
+message = input("Enter your message below: \n")
+s.send(message.encode('utf-8'))
 print("Request has been sent from client")
-response = s.recv(1024)
+response = s.recv(1024).decode('utf-8')
 print("Response has been received from server")
-print("Response: ", response)
+if response == "":
+	print("Exited program and closed server successfully!")
+else:
+	print("Response: ", response)
 s.close()
