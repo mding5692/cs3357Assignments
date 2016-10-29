ce4ca6487e3356e9819ca73d3bd16d418d5b9fdd
mding5692 <m.ding5692@gmail.com>
2016-10-29 17:21:28 -0400

    Asst3 update: added basic ack into files

--- Asst3/UDP_client.py
+++ Asst3/UDP_client.py
@@ -4,24 +4,50 @@ import struct
 import sys
 import hashlib
 
-UDP_IP = "127.0.0.1"
+UDP_IP = '127.0.0.1'
 UDP_PORT = 5005
 
-print("UDP target IP:", UDP_IP)
-print("UDP target port:", UDP_PORT)
+print('UDP target IP:', UDP_IP)
+print('UDP target port:', UDP_PORT)
 
-#Create the Checksum
-values = (0,0,b'TestData')
-UDP_Data = struct.Struct('I I 8s')
-packed_data = UDP_Data.pack(*values)
-chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+# Creates the information to be sent through
+raw_data_list = ['NCC-1701','NCC-1664','NCC-1017']
 
-#Build the UDP Packet
-values = (0,0,b'TestData',chksum)
-UDP_Packet_Data = struct.Struct('I I 8s 32s')
-UDP_Packet = UDP_Packet_Data.pack(*values)
+# Loops through raw_data_list and sends each raw_data
+for raw_data in raw_data_list:
+	#Assigns certain variables for ack and sequence number so easy to reference later on
+	ack = 0
+	seq = 0
 
-#Send the UDP Packet
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
+	#Create the Checksum
+	values = (ack,seq,raw_data)
+	UDP_Data = struct.Struct('I I 8s')
+	packed_data = UDP_Data.pack(*values)
+	chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding='UTF-8')
+
+	#Build the UDP Packet
+	values = (ack,seq,raw_data,chksum)
+	UDP_Packet_Data = struct.Struct('I I 8s 32s')
+	UDP_Packet = UDP_Packet_Data.pack(*values)
+
+	#Send the UDP Packet
+	sock = socket.socket(socket.AF_INET, # Internet
+	                     socket.SOCK_DGRAM) # UDP
+	sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
+	print("UDP Packet has been sent.")
+
+	# Receives response from receiver
+	resp, server_addr = sock.recvfrom(4096)
+	print("Packet sent back from receiver: ", resp)
+
+	# Checks ack response and sends new packets based on that information
+	while resp == ack: 
+		sock = socket.socket(socket.AF_INET, # Internet
+	                     socket.SOCK_DGRAM) # UDP
+		sock.sendto(UDP_Packet, (UDP_IP, UDP_PORT))
+		print("UDP Packet has been resent.") # sends packet again if previous packet was corrupted
+		resp, server_addr = sock.recvfrom(4096) # receives new check
+		print("Packet sent back from receiver ", resp)
+
+# Close connection at the end
+sock.close()
--- Asst3/UDP_server.py
+++ Asst3/UDP_server.py
@@ -4,7 +4,7 @@ import struct
 import sys
 import hashlib
 
-UDP_IP = "127.0.0.1"
+UDP_IP = '127.0.0.1'
 UDP_PORT = 5005
 unpacker = struct.Struct('I I 8s 32s')
 
@@ -13,20 +13,32 @@ unpacker = struct.Struct('I I 8s 32s')
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+print("Server set-up and listening at: ", UDP_IP)
 
 while True:
     #Receive Data
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     UDP_Packet = unpacker.unpack(data)
-    print("received from:", addr)
-    print("received message:", UDP_Packet)
+
+    # Prints out received UDP packet
+    print('received from:', addr)
+    print('received message:', UDP_Packet)
+
+    # assigns variables to hold ack and sequence numbers to make it easy to reference later
+    ack = UDP_Packet[0]
+    seq = UDP_Packet[1]
+
     #Create the Checksum for comparison
-    values = (UDP_Packet[0],UDP_Packet[1],UDP_Packet[2])
+    values = (ack,seq,UDP_Packet[2])
     packer = struct.Struct('I I 8s')
     packed_data = packer.pack(*values)
-    chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding="UTF-8")
+    chksum =  bytes(hashlib.md5(packed_data).hexdigest(), encoding='UTF-8')
+    
     #Compare Checksums to test for corrupt data
     if UDP_Packet[3] == chksum:
         print('CheckSums Match, Packet OK')
+
     else:
         print('Checksums Do Not Match, Packet Corrupt')
+        # Sends previous Ack to receiver to show that is corrupted
+        sock.sendto(ack,addr)
